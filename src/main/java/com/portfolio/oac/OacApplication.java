d507fe9a2591360ab4925f68710cc433e9ffa945
Antonia Obejero <109988399+AntoOb0@users.noreply.github.com>
2023-04-27 16:04:08 -0300

    corrección de api en backend

---
+++ src/main/java/com/portfolio/oac/OacApplication.java
@@ -0,0 +1,14 @@
+package com.portfolio.oac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OacApplication.class, args);
+	}
+
+}
+
--- src/main/java/com/portfolio/oac/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/oac/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://frontendmds.web.app")//prueba con link de firebase
+@CrossOrigin(origins = "https://frontendoac.web.app")//prueba con link de firebase
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
